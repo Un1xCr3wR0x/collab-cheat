9911f5cbe7a66993eb8c644cf1d26f110c611355
PyWarden <miskajfrefi@mail.ru>
2025-01-05 21:32:27 +0300

    update for scanning

--- Main.cpp
+++ Main.cpp
@@ -1,40 +1,93 @@
 #include <Windows.h>
+#include <Psapi.h>  // Äëÿ GetModuleInformation
 #include <iostream>
 #include <sstream>
+#include <vector>
 #include "./src/sdk/entities/CGameEntitySystem.h"
 #include "./src/sdk/entities/CBaseEntity.h"
 #include "./src/sdk/entities/Chandle.h"
 #include "./src/sdk/entities/CDOTABaseAbility.h"
 #include "./src/sdk/entities/CDOTABaseNPC.h"
+#include "./src/sdk/base/Vector.h"
+
+#pragma comment(lib, "Psapi.lib")  // Linking with Psapi.lib
+
+// Define the function type PrepareUnitOrders
+typedef void(__fastcall* _PrepareUnitOrders)(CBaseEntity* player, int orderType, int entHandle, Vector* movePosition,
+    int abilityIndex, int orderIssuer, CBaseEntity* entity,
+    bool queue, int queueBehavior);
+_PrepareUnitOrders PrepareUnitOrders = nullptr;
+
+// Function for searching for a signature in memory
+uintptr_t FindSignature(HMODULE module, const std::vector<BYTE>& pattern) {
+    MODULEINFO modinfo = { 0 };
+    if (!GetModuleInformation(GetCurrentProcess(), module, &modinfo, sizeof(MODULEINFO))) {
+        return 0;
+    }
+
+    BYTE* data = reinterpret_cast<BYTE*>(modinfo.lpBaseOfDll);
+    const size_t size = modinfo.SizeOfImage;
+
+    for (size_t i = 0; i < size - pattern.size() + 1; i++) {
+        bool found = true;
+        for (size_t j = 0; j < pattern.size(); j++) {
+            if (data[i + j] != pattern[j]) {
+                found = false;
+                break;
+            }
+        }
+        if (found) {
+            return reinterpret_cast<uintptr_t>(&data[i]);
+        }
+    }
+    return 0;
+}
 
 template <typename T>
 T* GetInterface(const char* name, const char* library) {
     const auto handle = GetModuleHandle(library);
-
     if (!handle) {
         return nullptr;
     }
-
     const auto functionAddress = GetProcAddress(handle, "CreateInterface");
-
     if (!functionAddress)
         return nullptr;
-
     using Fn = T * (*)(const char*, int*);
-
     const auto CreateInterface = reinterpret_cast<Fn>(functionAddress);
-
     if (!CreateInterface) {
         return nullptr;
     }
-
     return CreateInterface(name, nullptr);
-};
+}
 
 void HackThread(HMODULE instance) {
     AllocConsole();
     FILE* file;
     freopen_s(&file, "CONOUT$", "w", stdout);
+
+    // signature from x64dbg
+    std::vector<BYTE> signature = {
+        0x4C, 0x89, 0x4C, 0x24, 0x20, 0x44, 0x89, 0x44, 0x24, 0x18,
+        0x89, 0x54, 0x24, 0x10, 0x48, 0x89, 0x4C, 0x24, 0x08, 0x55,
+        0x53, 0x56, 0x57, 0x41, 0x54, 0x48, 0x8D, 0x6C, 0x24, 0xF0
+    };
+
+    // get adress client.dll
+    HMODULE clientDll = GetModuleHandleA("client.dll");
+    if (!clientDll) {
+        std::cout << "client.dll not found" << std::endl;
+        return;
+    }
+
+    // searching signature
+    uintptr_t prepareUnitOrdersAddress = FindSignature(clientDll, signature);
+    if (prepareUnitOrdersAddress == 0) {
+        std::cout << "Sign not found" << std::endl;
+        return;
+    }
+
+    std::cout << "adress PrepareUnitOrders: 0x" << std::hex << prepareUnitOrdersAddress << std::endl;
+    PrepareUnitOrders = reinterpret_cast<_PrepareUnitOrders>(prepareUnitOrdersAddress);
 
     while (!GetAsyncKeyState(VK_END)) {
         if (GetAsyncKeyState(VK_INSERT) & 1) {
@@ -43,76 +96,26 @@ void HackThread(HMODULE instance) {
                     "CreateInterface"))("GameResourceServiceClientV001", nullptr);
             CGameEntitySystem* gameEntitySystem = *(CGameEntitySystem**)((uintptr_t)GameResourceServiceClient + 0x58);
             std::cout << "GetHighestEntityIndex: " << gameEntitySystem->GetHighestEntityIndex() << std::endl;
-            
-            for (std::size_t index = 0; index <= 64; ++index)
-            {
-                CBaseEntity* Entity = gameEntitySystem->GetEntity(index);
-                if (Entity && IsValidReadPtr(Entity))
-                {
-                    constexpr auto offset_m_iszPlayerName = 0x650;
-                    constexpr auto offset_m_hAssignedHero = 0x80c;
-                    constexpr int offset_m_iTaggedAsVisibleByTeam = 0xc7c;
-                    constexpr auto offset_m_hInventory = 0xf30;
-                    constexpr auto offset_m_hAbilities = 0xae0;
-                    constexpr auto offset_m_fCooldown = 0x580;
-
-                    // Using CHandle from Chandle.h
-                    CHandle<CBaseEntity>* heroHandle = (CHandle<CBaseEntity>*)((uintptr_t)Entity + offset_m_hAssignedHero);
-                    CDOTABaseNPC* EntityHero = static_cast<CDOTABaseNPC*>(gameEntitySystem->GetEntity(heroHandle->Index()));
-
-                    if (EntityHero && IsValidReadPtr(EntityHero)) {
-                        // Get abilities using the GetAbilities method
-                        std::cout << "ABILITIES!!!!!!!!!!!" << std::endl;
-                        auto abilities = EntityHero->GetAbilities();
-                        for (const auto& abilityHandle : abilities) {
-                            if (abilityHandle.IsValid()) {
-                                auto ability = gameEntitySystem->GetEntity<CDOTABaseAbility>(abilityHandle.Index());
-                                if (ability && IsValidReadPtr(ability)) {
-                                    std::cout << "Ability Name: " << ability->GetIdentity()->GetName() << std::endl;
-                                    std::cout << "Ability Cooldown: " << ability->GetCooldown() << std::endl;
-                                    std::cout << "Ability Mana: " << ability->GetLevelSpecialValueFor<int>("AbilityManaCost") << std::endl;
-                                }
-                            }
-                        }
-						std::cout << "ITEEEEEEEEEEEEEEEEM!!!!!!!!!!!" << std::endl;
-						auto items = EntityHero->GetItems();
-                        for (const auto& itemHandle : items) {
-                            if (itemHandle.IsValid()) {
-                                auto item = gameEntitySystem->GetEntity<CDOTAItem>(itemHandle.Index());
-                                if (item && IsValidReadPtr(item)) {
-                                    std::cout << "Item Name: " << item->GetIdentity()->GetName() << std::endl;
-                                    std::cout << "Item Cooldown: " << item->GetCooldown() << std::endl;
-                                    std::cout << "Item Mana: " << item->GetLevelSpecialValueFor<int>("AbilityManaCost") << std::endl;
-                                }
-                            }
-                        }
-                    }
-
-
-                    if (EntityHero) {
-                        int EntityHeroTaggedAsVisibleByTeam = (int)((uintptr_t)EntityHero + offset_m_iTaggedAsVisibleByTeam);
-                        std::cout << "Entity: " << Entity << std::endl;
-                        std::cout << "Entity Health : " << EntityHero->GetHealth() << std::endl;
-                        std::cout << "Entity Max Health : " << EntityHero->GetMaxHealth() << std::endl;
-                        std::cout << "Entity Name : " << (char*)((uintptr_t)Entity + offset_m_iszPlayerName) << std::endl;
-                        std::cout << "Entity Assigned Hero : " << heroHandle->Index() << std::endl;
-                        std::cout << "EntityHero: " << EntityHero << std::endl;
-                        std::cout << "is visible: " << EntityHeroTaggedAsVisibleByTeam << std::endl;
-
-                        std::stringstream ss;
-                        ss << "player " << (char*)((uintptr_t)Entity + offset_m_iszPlayerName) 
-                           << "(" << Entity << ") controls hero indexed " 
-                           << heroHandle->Index() << "\n";
-                        OutputDebugStringA(ss.str().c_str());
-                    }
-                }
+            CBaseEntity* localPlayer = gameEntitySystem->GetEntity(1);
+            if (localPlayer && PrepareUnitOrders) {
+                Vector movePosition(100.0f, 100.0f, 100.0f);  
+                PrepareUnitOrders(localPlayer,
+                    1,
+                    0,
+                    &movePosition,
+                    0,
+                    0,
+                    localPlayer,
+                    false,
+                    0);
+                std::cout << "Hero going." << std::endl;
             }
         }
         Sleep(200);
     }
+
     if (file)
         fclose(file);
-
     FreeConsole();
     FreeLibraryAndExitThread(instance, 0);
 }
@@ -120,9 +123,7 @@ void HackThread(HMODULE instance) {
 BOOL WINAPI DllMain(HMODULE instance, DWORD reason, LPVOID reserved) {
     if (reason == DLL_PROCESS_ATTACH) {
         DisableThreadLibraryCalls(instance);
-
         const auto thread = CreateThread(nullptr, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(HackThread), instance, 0, nullptr);
-
         if (thread) {
             CloseHandle(thread);
         }
